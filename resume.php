74668360c046c69a71d359804b41f393e59626e2
Antonio Abella <antonio.abella@smartbear.com>
2024-05-24 19:03:00 -0400

    update may 2024

--- resume.php
+++ resume.php
@@ -3,7 +3,7 @@
 <div class="bodybody" style="margin-top:0.3vh">
 <div class="resumebody">
         <div>
-	    <div style="text-align: right; font-size:11pt;">Save a copy:
+        <div style="text-align: right; font-size:11pt;">Save a copy:
                 &nbsp;
                 <a href="resumes/resume-antonio-abella.pdf" target="_blank" class="projlink" rel="noopener" style="font-weight:600">pdf</a>
                 &nbsp;|&nbsp;
@@ -27,22 +27,21 @@
                         Skills:
                     </div>
                     <ul>
-                        <li>Expertise in:</li>
+                        <li><b>Expertise</b> in:</li>
                         <ul>
-                            <li><u>Cloud Computing:</u> Amazon AWS (EC2, Route53, RDS, S3, VPC, ACM, CloudWatch)</li>
-                            <li><u>Operating Systems:</u> Ubuntu 18.04, 20.04; CentOS 7</li>
-                            <li><u>Programming Languages:</u> Python, Shell (posix sh and bash), PowerShell</li>
-                            <li><u>Software & Tools:</u> Docker, Git, Ansible, Terraform, Jenkins, Grafana, InfluxDB, Zabbix</li>
-                            <li><u>Hardware:</u> Dell PowerEdge and HP ProLiant servers</li>
+                            <li><b>Cloud Computing:</b> Amazon AWS (CloudWatch, EC2, EC2 Image Builder, ElastiCache, Lambda, RDS, Route53, S3, SNS, VPC, Boto3)</li>
+                            <li><b>Operating Systems:</b> Ubuntu 16 – 22, CentOS 6 – 7, AlmaLinux 9</li>
+                            <li><b>Programming Languages:</b> Python, Shell (sh/bash), PowerShell</li>
+                            <li><b>Software & Tools:</b> Ansible, Docker, Git/GitHub, GH Actions, Grafana, InfluxDB, Jenkins, Terraform, Zabbix</li>
                         </ul>
-                        <li>Some experience with:</li>
+                        <li><b>Some experience</b> with:</li>
                         <ul>
-                            <li><u>Cloud Computing:</u> Amazon AWS (IAM, EKS, Lambda, DynamoDB)</li>
-                            <li><u>Programming Languages:</u> Groovy, Java, SQL, PHP, Javascript, Perl</li>
-                            <li><u>Software & Tools:</u> MySQL/MariaDB, MongoDB, RabbitMQ, Memcached, NGINX, Make</li>
-                            <li><u>Hardware:</u> Juniper and Cisco routers and firewalls, Cisco and HP managed switches</li>
+                            <li><b>Cloud Computing:</b> Amazon AWS (API Gateway, DocumentDB, DynamoDB, EKS, Elastic Beanstalk, IAM)</li>
+                            <li><b>Programming Languages:</b> Groovy, Java, Javascript, Perl, PHP, SQL</li>
+                            <li><b>Software & Tools:</b> ElasticSearch, Kafka, Kubernetes, MariaDB/MySQL, Memcached, MongoDB, NGINX, RabbitMQ, Traefik</li>
                         </ul>
-                        <li>Additionally experienced with Windows Server 2012R2, VMware ESXi 6.0+, Active Directory</li>
+                        <li>Additionally experienced with Windows Server 2012 - 2022, VMware ESXi 6.0+, Active Directory</li>
+                        <li>Some experience with Dell PowerEdge and HP ProLiant servers, Juniper and Cisco routers and firewalls, and Cisco and HP managed switches</li>
                         <li>Fully fluent in written and verbal English and Spanish</li>
                     </ul>
                 </div>
@@ -55,13 +54,13 @@
                 <div style="float: right">
                     <div style="font-weight: 600;">September 2021 - present</div>
                 </div>
-		<br />
+        <br />
                 <div style="float: left">
                     <div style="font-size: 0.85em; font-style: italic;">DevOps Engineer</div>
                 </div>
                 <div style="float: right">
                     <div style="font-size: 0.85em; font-style: italic;">July 2019 - September 2021</div>
-		</div>
+        </div>
                 <br />
                 <div style="float: left">
                     <div style="font-size: 0.85em; font-style: italic;">Junior DevOps Engineer</div>
@@ -72,10 +71,13 @@
                 <br style="clear: both" />
                 <p>
                     <ul>
-                        <li>Designed and implemented a dynamic iptables firewall configuration generator to improve reconfiguration performance on a cluster of 300 hosts. This implementation yielded a 99.95% improvement in runtime over the previous solution, resulting in over 12 hours saved per deployment.</li>
-                        <li>Built and administered a self-hosted InfluxDB Enterprise time-series database cluster hosting core application data. Oversaw migration of the entire data lifecycle from the previous solution. Worked with the development team to carry out performance and load benchmarking at production scale.</li>
-                        <li>Migrated core application legacy Docker Swarm clusters to latest Docker Swarm Mode. Defined cluster infrastructure in Terraform for reproducible provisioning and disaster recovery. Conducted staged rollout of new Swarm environments in coordination with development and QA teams. Reconstructed all related CI/CD pipelines to interface with new Docker and Swarm versions.</li>
-                        <li>Engineered platform for self-service provisioning and deployment of transient development environments within Docker Swarm. Extended Terraform configuration to source dynamic environment metadata from a central database. Provided the development team with the tools to accelerate testing and facilitate code comparisons.</li>
+                        <li>Architected a platform enabling automated provisioning and deployment of ephemeral development and demonstration environments within Docker Swarm. Enhanced Terraform configuration to dynamically fetch environment metadata from a configuration database, greatly accelerating the development team’s capacity for functional testing and enabling live cross-comparisons of feature branches.</li>
+                        <li>Designed, coded, and maintain a highly performant and stable customer-facing serverless API in Python on AWS Lambda, integrated with AWS API Gateway, for seamless request, generation, and management of SaaS product demo environments. Reduced provisioning time from over 20 minutes to under 60 seconds, replacing a much slower and unreliable legacy system.</li>
+                        <li>Architected and implemented a secure AWS AMI build and deployment pipeline for ephemeral CI agent machine images, enabling headless functional testing of interactive Windows desktop applications with automated rebuilding for latest operating system and software updates.</li>
+                        <li>Developed and deployed a dynamic `iptables` firewall configuration generator in Python to improve reconfiguration performance on a cluster of nearly 300 hosts. This implementation replaced a much slower legacy solution, and reduced runtimes from nearly 12 hours to under 5 minutes per cluster-wide deployment.</li>
+                        <li>Built and provide primary support and administration for a self-hosted production InfluxDB Enterprise time-series database cluster hosting core application data. Oversaw migration of entire customer-facing data lifecycle from previous MySQL-based solution. Cooperated with the development team to carry out performance benchmarking and load tests at production scale.</li>
+                        <li>Migrated core application legacy Docker Swarm clusters to latest Docker Swarm Mode. Defined cluster infrastructure in Terraform for reproducible provisioning and disaster recovery. Planned configuration to allow continuous, non-disruptive upgrades of cluster nodes. Designed and implemented AMI build pipeline for the automated configuration and upgrade of cluster node software. Configured all-new observability suites for monitoring and alerting on cluster and application health. Led and conducted staged rollout of new Swarm environments in coordination with development and QA teams. Reconstructed all related CI/CD pipelines to interface with new Docker and Swarm versions.</li>
+                        <li>Researched and rolled out centralized log aggregation solution on Elastic Cloud for core application container logs and metrics, providing greatly increased insight and awareness into current and historic platform and application health.</li>
                     </ul>
                 </p>
                 <div style="font-weight: 700">Inktel Contact Center Solutions</div>
